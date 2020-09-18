9625e513d9eed4e77b144b6fffa7b27300470592
Ethan Bailey <ebailey256@gmail.com>
2020-09-17 21:35:45 -0400

    smaller eslintrc

--- files/eslintrc.js
+++ files/eslintrc.js
@@ -1,16 +1,7 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    env: {
-        "browser": true,
-        "node": true,
-    },
-    plugins: [
-        '@typescript-eslint',
-    ],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
-    ]
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ]
 }
