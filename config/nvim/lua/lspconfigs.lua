bc96f56c0e7a891879aa57e2d1012a8f08f4b0cf
Ethan Bailey <ebailey256@gmail.com>
2020-12-28 14:04:55 -0500

    vim-dispatch, lsp tweaks

--- config/nvim/lua/lspconfigs.lua
+++ config/nvim/lua/lspconfigs.lua
@@ -1,7 +1,14 @@
+local cmd = vim.cmd
 local lsp = require 'lspconfig'
 local ts = require 'nvim-treesitter.configs'
 
+-- treesitter
+ts.setup { highlight = { enable = true } }
+
+-- js
 lsp.flow.setup{ on_attach = require'completion'.on_attach }
+
+-- lua
 lsp.sumneko_lua.setup{ on_attach = require'completion'.on_attach, settings = {
     Lua = {
         diagnostics = {
@@ -13,6 +20,12 @@ lsp.sumneko_lua.setup{ on_attach = require'completion'.on_attach, settings = {
         },
     },
 }}
+cmd 'au Filetype lua setlocal omnifunc=v:lua.vim.lsp.omnifunc'
+
+-- python
 lsp.pyright.setup{ on_attach = require'completion'.on_attach }
+cmd 'au Filetype python setlocal omnifunc=v:lua.vim.lsp.omnifunc'
+
+-- viml
 lsp.vimls.setup{ on_attach = require'completion'.on_attach }
-ts.setup { highlight = { enable = true } }
+cmd 'au Filetype vim setlocal omnifunc=v:lua.vim.lsp.omnifunc'
