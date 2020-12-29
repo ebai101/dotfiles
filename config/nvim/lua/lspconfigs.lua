bbc20b69871c36e508f494ed50678e082c694b72
Ethan Bailey <ebailey256@gmail.com>
2020-12-29 17:51:19 -0500

    karabiner, pyright/lua settings, smarttab

--- config/nvim/lua/lspconfigs.lua
+++ config/nvim/lua/lspconfigs.lua
@@ -10,20 +10,31 @@ lsp.flow.setup{ on_attach = require'completion'.on_attach }
 
 -- lua
 lsp.sumneko_lua.setup{ on_attach = require'completion'.on_attach, settings = {
-    Lua = {
-        diagnostics = {
-            globals = {
-                'vim',
-                'hs',
-                'spoon'
+        Lua = {
+            runtime = {
+                version = 'Lua 5.1',
+            },
+            diagnostics = {
+                globals = {
+                    'vim',
+                    'hs',
+                    'spoon'
+                },
             },
         },
-    },
 }}
 cmd 'au Filetype lua setlocal omnifunc=v:lua.vim.lsp.omnifunc'
 
 -- python
-lsp.pyright.setup{ on_attach = require'completion'.on_attach }
+lsp.pyright.setup{ on_attach = require'completion'.on_attach, settings = {
+        python = {
+            analysis = {
+                autoSearchPaths = true,
+                autoImportCompletions = true,
+                useLibraryCodeForTypes = true,
+            },
+        },
+}}
 cmd 'au Filetype python setlocal omnifunc=v:lua.vim.lsp.omnifunc'
 
 -- viml
