2bc48b8739d5a31a4cad1e3bc60fea6b8a989f9c
Ethan Bailey <ebailey256@gmail.com>
2021-02-02 19:18:22 -0500

    diagnosticls, clangd, sumneko fixes, rls, tsserver

--- config/nvim/lua/lspsetup.lua
+++ config/nvim/lua/lspsetup.lua
@@ -4,12 +4,16 @@ local ts = require 'nvim-treesitter.configs'
 -- treesitter
 ts.setup { ensure_installed = 'maintained', highlight = { enable = true } }
 
--- lua
-local sumneko_root_path = '~/dev/lua-language-server'
-local sumneko_binary = sumneko_root_path..'/bin/Linux'..'/lua-language-server'
+-- clangd
+lsp.clangd.setup{ on_attach = require'completion'.on_attach }
 
-lsp.sumneko_lua.setup{ on_attach = require'completion'.on_attach,
-    cmd = {sumneko_binary, '-E', sumneko_root_path..'/main.lua'},
+-- lua
+local sumneko_root = '~/dev/lua-language-server'
+local sumneko_binary = sumneko_root..'/bin/Linux/lua-language-server'
+
+lsp.sumneko_lua.setup{
+    on_attach = require'completion'.on_attach,
+    cmd = {sumneko_binary, '-E', sumneko_root..'/main.lua'},
     settings = {
         Lua = {
             runtime = {
@@ -38,6 +42,67 @@ lsp.pyright.setup{ on_attach = require'completion'.on_attach, settings = {
 
 -- viml
 lsp.vimls.setup{ on_attach = require'completion'.on_attach }
+
+-- tsserver
+lsp.tsserver.setup{ on_attach = require'completion'.on_attach }
+
+-- rust
+lsp.rls.setup{ on_attach = require'completion'.on_attach }
+
+-- diagnosticls
+lsp.diagnosticls.setup{
+    on_attach = require'completion'.on_attach,
+    filetypes = {"javascript", "javascriptreact", "typescript", "typescriptreact"},
+    init_options = {
+        linters = {
+            eslint = {
+                sourcename = "eslint",
+                command = './node_modules/.bin/eslint',
+                rootPatterns = { '.git' },
+                debounce = 100,
+                args = { '--stdin', '--stdin-filename', '%filepath', '--format', 'json' },
+                sourceName = 'eslint',
+                parseJson = {
+                    errorsRoot = '[0].messages',
+                    line = 'line',
+                    column = 'column',
+                    endLine = 'endLine',
+                    endColumn = 'endColumn',
+                    message = '${message} [${ruleId}]',
+                    security = 'severity'
+                },
+                securities = {
+                    [2] = 'error',
+                    [1] = 'warning'
+                }
+            }
+        },
+        filetypes = {
+            javascript = 'eslint',
+            javascriptreact = 'eslint',
+            typescript = 'eslint',
+            typescriptreact = 'eslint',
+        },
+        formatters = {
+            prettierEslint = {
+                command = './node_modules/.bin/prettier-eslint',
+                args = { '--stdin' },
+                rootPatterns = { '.git' },
+            },
+            prettier = {
+                command = './node_modules/.bin/prettier',
+                args = { '--stdin-filepath', '%filename' }
+            }
+        },
+        formatFiletypes = {
+            javascript = 'prettierEslint',
+            javascriptreact = 'prettierEslint',
+            json = 'prettier',
+            typescript = 'prettierEslint',
+            typescriptreact = 'prettierEslint'
+        }
+    }
+}
 
 -- gopls
 lsp.gopls.setup{
@@ -73,4 +138,3 @@ function lsp.organize_go_imports(timeoutms)
 
     vim.lsp.buf.formatting()
 end
-vim.cmd('au BufWritePre *.go lua require"lspconfig".organize_go_imports(1000)')
