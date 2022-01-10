80ecb60a0edf1045459c548a640c7ee6c085d2e6
Ethan Bailey <ebailey256@gmail.com>
2022-01-10 15:02:39 -0500

    fix for utilities occasionally not showing up

--- config/hammerspoon/Spoons/Reason.spoon/init.lua
+++ config/hammerspoon/Spoons/Reason.spoon/init.lua
@@ -84,25 +84,36 @@ function reason:rebuild()
     local menus = app:getMenuItems()[4]['AXChildren'][1]
     local newList = {}
 
+    -- for i=7, 9 do -- Create -> Instruments, Effects, Utilities
     for i=7, 9 do
-        for j=11, #menus[i]['AXChildren'][1] do
-            for k=1, #menus[i]['AXChildren'][1][j]['AXChildren'][1] do
-                if not(menus[i]['AXChildren'][1][j]['AXChildren'][1][k]['AXTitle'] == '') then
-                    local title = menus[i]['AXChildren'][1][j]['AXChildren'][1][k]['AXTitle']
-                    log.d(title)
-                    table.insert(newList, {
-                            ["text"] = title,
-                            ["subText"] = string.format('%s - %s',
-                                menus[i]['AXTitle'],
-                                menus[i]['AXChildren'][1][j]['AXTitle']
-                                ),
-                            ["menuSelector"] = {
-                                "Create",
-                                menus[i]['AXTitle'],
-                                menus[i]['AXChildren'][1][j]['AXTitle'],
-                                menus[i]['AXChildren'][1][j]['AXChildren'][1][k]['AXTitle'],
-                            }
-                        })
+        local foundSubmenu = false
+        for j=1, #menus[i]['AXChildren'][1] do
+            -- iterate until we find the first submenu, which is always Built-in Devices
+            local subtitle = menus[i]['AXChildren'][1][j]['AXTitle']
+            if subtitle == 'Built-in Devices' then
+                foundSubmenu = true
+            end
+            -- iterate thru the successive submenus
+            if foundSubmenu then
+                local submenu = menus[i]['AXChildren'][1][j]['AXChildren'][1]
+                for k=1, #submenu do
+                    if not(submenu[k]['AXTitle'] == '') then -- table contains divider bars, which have a blank title
+                        local title = submenu[k]['AXTitle']
+                        log.d(title, ': ', menus[i]['AXTitle'])
+                        table.insert(newList, {
+                                ["text"] = title,
+                                ["subText"] = string.format('%s - %s',
+                                    menus[i]['AXTitle'],
+                                    subtitle
+                                    ),
+                                ["menuSelector"] = {
+                                    "Create",
+                                    menus[i]['AXTitle'],
+                                    subtitle,
+                                    submenu[k]['AXTitle'],
+                                }
+                            })
+                    end
                 end
             end
         end
