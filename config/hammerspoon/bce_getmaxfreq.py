2c5b320a2dcc4e2191c34beab7901dbc32566a8d
Ethan Bailey <ebailey256@gmail.com>
2022-05-31 20:15:35 -0400

    print the first 50 items

--- config/hammerspoon/bce_getmaxfreq.py
+++ config/hammerspoon/bce_getmaxfreq.py
@@ -3,14 +3,12 @@
 import json
 
 with open('bce_freq.json', 'r') as f:
-    data = json.load(f)
+    data = sorted([(k, v) for k, v in json.load(f).items()],
+                  key=lambda x: x[1],
+                  reverse=True)
 
-maxtitle = ''
-max = 0
-for d in data:
-    num = data[d]
-    if (max < num):
-        max = num
-        maxtitle = d
-print(maxtitle)
-print(max)
+print('# | freq | device')
+index = 1
+for d in data[:50]:
+    print(f'{index}: {d[1]}\t- {d[0]}')
+    index += 1
