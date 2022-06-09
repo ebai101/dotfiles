561930a0380a0a70c17cf80b48e13b00ee20eda2
Ethan Bailey <ebailey256@gmail.com>
2022-06-09 13:17:14 -0400

    new window focuser

--- config/hammerspoon/windows.lua
+++ config/hammerspoon/windows.lua
@@ -2,70 +2,14 @@
 -- Hyper Windows
 -----------------------------------------------
 
-local apps = {}
-
--- different apps for different comps
-if hs.host.localizedName() == 'mbp' then
-    apps = {
-        {'e', 'Google Chrome',  false},
-        {'r', 'Messages',       true },
-        {'d', 'Alacritty',      false},
-        {'f', 'Mailspring',     true },
-        {'c', 'Discord',        true },
-        {'v', 'Finder',         true },
-        {'x', 'Spotify',        true },
-    }
-elseif hs.host.localizedName() == 'hackerman' then
-    apps = {
-        {'e', 'Google Chrome',      false},
-        {'r', 'Messages',           true },
-        {'d', 'Alacritty',          false},
-        {'f', 'Mailspring',         true },
-        {'c', 'Discord',            false},
-        {'v', 'Finder',             true },
-        {'x', 'Spotify',            true }
-    }
-end
-
-local function focuser(direction)
-    return function()
-        local current = hs.window.focusedWindow()
-        if not current then
-            hs.alert.show('No active window')
-            return
-        end
-
-        if     direction == 'north' then current:focusWindowNorth()
-        elseif direction == 'south' then current:focusWindowSouth()
-        elseif direction == 'east'  then current:focusWindowEast()
-        elseif direction == 'west'  then current:focusWindowWest()
-        end
-
-        for i=1, #apps do
-            if apps[i][2] == current:application():title() and apps[i][3] == true then
-                current:application():hide()
-                break
-            end
-        end
-
-        hs.mouse.setAbsolutePosition(hs.geometry.rectMidPoint(hs.window.focusedWindow():frame()))
-    end
-end
-
-local function moveWindowToDisplay(d)
-    return function()
-        hs.window.focusedWindow():moveToScreen(d, false, true)
-        hs.mouse.setAbsolutePosition(hs.geometry.rectMidPoint(hs.window.focusedWindow():frame()))
-    end
-end
-
--- focuser binds
--- hs.hotkey.bind(hyper,   'h', focuser('west'))
--- hs.hotkey.bind(hyper,   'j', focuser('south'))
--- hs.hotkey.bind(hyper,   'k', focuser('north'))
--- hs.hotkey.bind(hyper,   'l', focuser('east'))
 -- hs.hotkey.bind(shyper,  '1', moveWindowToDisplay("Acer XFA240"))
 -- hs.hotkey.bind(shyper,  '2', moveWindowToDisplay("R240HY"))
+-- local function moveWindowToDisplay(d)
+--     return function()
+--         hs.window.focusedWindow():moveToScreen(d, false, true)
+--         hs.mouse.setAbsolutePosition(hs.geometry.rectMidPoint(hs.window.focusedWindow():frame()))
+--     end
+-- end
 
 -- app launcher binds
 local k = hs.hotkey.modal.new(hyper, 'p')
@@ -94,19 +38,47 @@ function k:exited()
     kActive = false
 end
 
+local function focus(appName, appFloat)
+    local frontApp = hs.application.frontmostApplication()
+    if frontApp:title() == appName then
+        if appFloat then frontApp:hide() end
+    else
+        hs.application.launchOrFocus(appName)
+    end
+end
+
+local apps = {
+    {'e', function()
+        focus('Google Chrome', false)
+    end},
+    {'r', function()
+        focus('Messages', true)
+    end},
+    {'d', function()
+        focus('Alacritty', false)
+    end},
+    {'f', function()
+        -- open gmail in browser
+        hs.execute('/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome "chrome-extension://oeopbcgkkoapgobdbedcemjljbihmemj/popup.html"')
+    end},
+    {'c', function()
+        focus('Discord', false)
+    end},
+    {'v', function()
+        focus('Finder', true)
+    end},
+    {'x', function()
+        focus('Calendar', true)
+    end}
+}
+
 for i = 1, #apps do
     local appHotkey = apps[i][1]
-    local appName = apps[i][2]
-    local appFloat = apps[i][3]
+    local appFunc = apps[i][2]
 
     k:bind({}, appHotkey, function()
         hs.alert.closeAll()
-        local frontApp = hs.application.frontmostApplication()
-        if frontApp:title() == appName then
-            if appFloat then frontApp:hide() end
-        else
-            hs.application.launchOrFocus(appName)
-        end
+        appFunc()
         k:exit()
     end)
 end
