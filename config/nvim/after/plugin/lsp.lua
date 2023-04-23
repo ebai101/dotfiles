4ccded2e4560b4cb2d2527ced1f84436f5748dac
Ethan Bailey <ebailey256@gmail.com>
2023-04-23 03:18:52 -0400

    lsp/treesitter fixes

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -7,7 +7,7 @@ lsp.ensure_installed({
     'pyright'
 })
 
-lsp.configure('sumneko_lua', {
+lsp.configure('lua_ls', {
     settings = {
         Lua = {
             diagnostics = {
--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-	ensure_installed = { "python", "javascript", "lua", "help" },
+	ensure_installed = { "python", "javascript", "lua" },
 	sync_install = false,
 	auto_install = true,
 	highlight = {
