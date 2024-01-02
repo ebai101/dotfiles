76cc24a166e53f1d7c5425863b70ffc3056b6c3f
Ethan Bailey <ebailey256@gmail.com>
2024-01-02 18:51:16 -0500

    vanilla prompt update

--- files/prompt.sh
+++ files/prompt.sh
@@ -1,7 +1,47 @@
-function remote_hostname() {
-    if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-        echo "$(tput setaf 4)🌐 $(hostname)$(tput sgr0) in "
-    fi
+[ -n "$ZSH_VERSION" ] && {
+    precmd() {
+        local _git=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}')
+        PROMPT=''
+        if [[ $_git == "" ]]; then
+            PROMPT+="%F{blue}%3~ "
+        else
+            PROMPT+="%F{blue}%1d "
+            PROMPT+="%F{magenta}${_git} "
+        fi
+        PROMPT+='%(?.%F{green}.%F{red})%(!.#.$)%f '
+    }
 }
 
-PS1="$(remote_hostname)$(tput setaf 6)%1/$(tput sgr0)$(tput setaf 2) ❯$(tput sgr0) "
+[ -n "$BASH_VERSION" ] && {
+    __prompt_command() {
+        local _exit="$?"
+        local _green="\[$(tput setaf 2)\]"
+        local _blue="\[$(tput setaf 4)\]"
+        local _red="\[$(tput setaf 1)\]"
+        local _purple="\[$(tput setaf 5)\]"
+        local _reset="\[$(tput sgr0)\]"
+        local _git=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}')
+
+        PS1=''
+        if [[ $_git == "" ]]; then
+            PS1+="${_blue}"
+            PS1+="${PWD#"${PWD%/*/*/*}/"} "
+            PS1+="${_reset}"
+        else
+            PS1+="${_blue}"
+            PS1+="${PWD#"${PWD%/*}/"} "
+            PS1+="${_reset}"
+            PS1+="${_purple}"
+            PS1+="$_git "
+            PS1+="${_reset}"
+        fi
+
+        if [ $_exit != 0 ]; then
+            PS1+="${_red}\$${_reset} "
+        else
+            PS1+="${_green}\$${_reset} "
+        fi
+    }
+
+    PROMPT_COMMAND=__prompt_command
+}
