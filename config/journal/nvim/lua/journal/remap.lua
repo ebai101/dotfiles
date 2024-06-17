33a85b4dd3377f48845bdaf71316726e54feb929
Ethan Bailey <ebailey256@gmail.com>
2024-06-16 23:42:30 -0400

    even more formatting

--- config/journal/nvim/lua/journal/remap.lua
+++ config/journal/nvim/lua/journal/remap.lua
@@ -26,18 +26,18 @@ vim.keymap.set('n', '<leader>d', '\"_d')
 vim.keymap.set('v', '<leader>d', '\"_d')
 
 -- replace word under cursor
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- make the current file executable
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
 
 -- netrw
-vim.keymap.set('n', "<leader>pv", vim.cmd.Lexplore)
+vim.keymap.set('n', '<leader>pv', vim.cmd.Lexplore)
 vim.api.nvim_create_autocmd('filetype', {
     pattern = 'netrw',
     callback = function()
-        vim.keymap.set('n', 'j', 'jp', {remap = true, buffer = true})
-        vim.keymap.set('n', 'k', 'kp', {remap = true, buffer = true})
-        vim.keymap.set('n', '<CR>', '<CR>:Lexplore<CR>', {remap = true, buffer = true})
+        vim.keymap.set('n', 'j', 'jp', { remap = true, buffer = true })
+        vim.keymap.set('n', 'k', 'kp', { remap = true, buffer = true })
+        vim.keymap.set('n', '<CR>', '<CR>:Lexplore<CR>', { remap = true, buffer = true })
     end
 })
